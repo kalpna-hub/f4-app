b9f81e2bd3bef29e66a0c23177de9513c387a7f9
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-07-04 16:09:09 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../App.css'
+import { Outlet } from "react-router-dom";
+const Header=()=>{
+    return(
+       <>
+       <header className="header">
+            <h3 className="header-title">
+                Social Media App
+            </h3>
+        </header>
+        <Outlet></Outlet>
+    </>
+    )
+}
+export default Header;
